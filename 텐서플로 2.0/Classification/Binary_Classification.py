9dabc297046d241699d6382d5932f81d3623ad0c
son-yu-seoung <65440674+son-yu-seoung@users.noreply.github.com>
2021-06-23 03:23:59 +0900

    이진 분류
    .

--- 텐서플로 2.0/Classification/Binary_Classification.py
+++ 텐서플로 2.0/Classification/Binary_Classification.py
@@ -4,6 +4,7 @@
 # 와인 데이터셋은 보스턴 주택 데이터세과 달리 외부에서 데이터를 불러오고 정제해야 함
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 red = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv', sep=';')
 white = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv', sep=';')
 # 12개의 속성으로 구성되어 있음
@@ -33,9 +34,54 @@ print(wine_norm.head())
 print(wine_norm.describe)
 
 # 정규화된 데이터를 랜덤하게 섞고 학습을 위해 numpy로 변환하기 
-p.113
+wine_shuffle = wine_norm.sample(frac=1) # 전체 데이터 중 frac의 비율만큼(100%)
+wine_np = wine_shuffle.to_numpy()
 
+# X, Y 분리, 학습 데이터와 테스트 데이터 분리
+import tensorflow as tf
+train_idx = int(len(wine_np) * 0.8)
+train_X, train_Y = wine_np[:train_idx, :-1], wine_np[:train_idx, -1]
+test_X, test_Y = wine_np[train_idx:, :-1], wine_np[train_idx:, -1]
 
+# tf.keras.utils에서 불러오는 to_categorical은 분류 문제에 자주 쓰이는 함수
+# 정답 행렬을 원-핫 인코딩 방식으로 바꿔줌
+train_Y = tf.keras.utils.to_categorical(train_Y, num_classes=2)
+test_Y = tf.keras.utils.to_categorical(test_Y, num_classes=2)
 
+# 모델 학습
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=48, activation='relu', input_shape=(12,)),
+    tf.keras.layers.Dense(units=24, activation='relu'),
+    tf.keras.layers.Dense(units=12, activation='relu'),
+    # 활성화 함수로 softmax를 사용하면 총합이 1.0인 확률로 변환된다.
+    # 큰 값을 강화하고 낮은 값을 약화하는 특성 !!!
+    tf.keras.layers.Dense(units=2, activation='softmax')
+])
+# categorical_crossentropy(CCE)
+# loss는 손실 값을 표현하 듯 CCE는 불확실성을 나타낸다. CCE = 0.06 -> Accuracy = 0.94
+model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.07), loss='categorical_crossentropy',
+metrics=['accuracy']) # 분류 문제는 정확도로 퍼포먼스를 측정하기 때문에 정확도 설정은 필수!
+model.summary()
 
+history = model.fit(train_X, train_Y, epochs=40, batch_size=32, validation_split=0.25)
 
+# 시각화
+plt.figure(figsize=(12, 4))
+
+plt.subplot(1, 2, 1)
+plt.plot(history.history['loss'], 'b-', label='loss')
+plt.plot(history.history['val_loss'], 'r--', label='val_loss')
+plt.xlabel('Epoch')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(history.history['accuracy'], 'g-', label='accuracy')
+plt.plot(history.history['val_accuracy'], 'k--', label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylim(0.7, 1)
+plt.legend()
+
+plt.show()
+
+# 모델 성능 평가
+model.evaluate(test_X, test_Y)
