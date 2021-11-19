4abf28e443fda438a91c817e5096013f1bc9ba81
son-yu-seoung <thsdbtjd10@naver.com>
2021-11-19 17:36:58 +0900

    텐서플로 2.0 책 실습

---
+++ RNN/실습/tensorflow_2.0_실습.py
@@ -0,0 +1,28 @@
+import numpy as np
+import tensorflow as tf
+
+x = []
+y = [] 
+
+for i in range(6):
+    lst = list(range(i, i+4))
+
+    x.append(list(map(lambda c: [c/10], lst)))
+    y.append(((i+4)/10))
+
+X = np.array(x) # Numpy version error -> Numpy down grade 1.19.5
+Y = np.array(y)
+
+for i in range(len(Y)):
+    print(f'{X[i]}, {Y[i]}')
+
+model = tf.keras.Sequential([
+    tf.keras.layers.SimpleRNN(units=10, return_sequences=False, input_shape=[4, 1]),
+    tf.keras.layers.Dense(1)
+])
+
+model.compile(optimizer='adam', loss='mse')
+model.summary()
+
+model.fit(X, Y, epochs=1000)
+print(model.predict(X))
